67d13f64e08fbd5fc729c46ea18a4533c2e91aba
mnurdin <muhamadnurdin141@gmail.com>
2019-08-09 18:30:54 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from './components/layout/Layout';
+import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+      <div>
+          <Layout>
+
+          </Layout>
+      </div>
+  );
+}
+
+export default App;
