c164ad8f11ef91745d9ca019facc4d054f2e8b11
ajiprays <ajiprays@users.noreply.github.com>
2019-08-16 16:57:32 +0700

    pull dari agung

---
+++ build/precache-manifest.358f9e1c113a463777a0349ec8b15652.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e1ef7cb0feb96bd266309441952212",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a12be1bae01c975cdd97",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "d623c6ff7432ada44437",
+    "url": "/static/js/2.8fcda840.chunk.js"
+  },
+  {
+    "revision": "a12be1bae01c975cdd97",
+    "url": "/static/js/main.3f2f1dad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
